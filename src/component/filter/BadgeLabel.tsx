4ab1652212ae00334154151fa90adb9452ef4310
Onur Sumer <s.onur.sumer@gmail.com>
2019-10-15 06:45:45 -0400

    removed checkboxes from protein impact type selector
    Signed-off-by: Onur Sumer <s.onur.sumer@gmail.com>

--- src/component/filter/BadgeLabel.tsx
+++ src/component/filter/BadgeLabel.tsx
@@ -6,36 +6,75 @@ export type BadgeLabelProps = {
     badgeContent?: number | string;
     badgeStyleOverride?: CSSProperties;
     badgeClassName?: string;
+    badgeFirst?: boolean;
 };
 
 export const DEFAULT_BADGE_STYLE = {
     color: "#FFF",
-    backgroundColor: "#000",
-    marginLeft: 5,
+    backgroundColor: "#000"
 };
 
 export class BadgeLabel extends React.Component<BadgeLabelProps, {}>
 {
     public static defaultProps: Partial<BadgeLabelProps> = {
-        badgeClassName: "badge"
+        badgeClassName: "badge",
+        badgeFirst: false
     };
+
+    protected get badgeStyle()
+    {
+        if (this.props.badgeFirst) {
+            return {
+                ...DEFAULT_BADGE_STYLE,
+                marginRight: 5,
+            };
+        }
+        else {
+            return {
+                ...DEFAULT_BADGE_STYLE,
+                marginLeft: 5,
+            };
+        }
+    }
+
+    protected get badge(): JSX.Element
+    {
+        return (
+            <span
+                className={this.props.badgeClassName}
+                style={{
+                    ...this.badgeStyle,
+                    ...this.props.badgeStyleOverride
+                }}
+            >
+                {this.props.badgeContent}
+            </span>
+        );
+    }
+
+    protected get badgeFirst(): JSX.Element
+    {
+        return (
+            <React.Fragment>
+                {this.badge}
+                {this.props.label}
+            </React.Fragment>
+        );
+    }
+
+    protected get badgeLast(): JSX.Element
+    {
+        return (
+            <React.Fragment>
+                {this.props.label}
+                {this.badge}
+            </React.Fragment>
+        );
+    }
 
     public render(): JSX.Element
     {
-        return (
-            <span>
-            {this.props.label}
-                <span
-                    className={this.props.badgeClassName}
-                    style={{
-                        ...DEFAULT_BADGE_STYLE,
-                        ...this.props.badgeStyleOverride
-                    }}
-                >
-                    {this.props.badgeContent}
-                </span>
-            </span>
-        );
+        return this.props.badgeFirst ? this.badgeFirst: this.badgeLast;
     }
 }
 
